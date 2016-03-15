d05deadb83d64310cb39b5674eaaa04c00c30d45
Baptiste Briois <baptiste.briois@hotmail.fr>
2016-03-15 16:07:41 +0100

    Add column admin to users table

--- database/migrations/2016_03_07_115232_add_columns_users.php
+++ database/migrations/2016_03_07_115232_add_columns_users.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsToPosts extends Migration
+class AddColumnsUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,6 +14,7 @@ class AddColumnsToPosts extends Migration
     {
         Schema::table('users', function(Blueprint $table) {
             $table->string('tel')->nullable()->after('name');
+            $table->boolean('admin')->default(false)->after('password');
         });
     }
 
