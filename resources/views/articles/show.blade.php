be32c62a300b0e9adf049e8ba9bc47ea99a4a211
CharlotteDupoux <charlottedupouxperso@hotmail.fr>
2016-03-17 12:13:06 +0100

    Modif

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,4 +5,20 @@
     <h3>Auteur: {{ $post->user->name }}</h3>
     <p>{{$post->content}}</p>
 
+    {!! Form::open(['route' => 'comment.store', 'method' => 'POST']) !!}
+    <div class="row">
+        <div class="col-md-10">
+            <div class="form-group">
+        {!! Form::text('title', null, [
+            'class' => 'form-control',
+            'placeholder' => 'Message...'
+        ]) !!}
+            </div>
+        </div>
+
+        <div class="col-md-2">
+            {!! Form::submit('Envoyer', ['class' => 'btn btn-block btn-success']) !!}
+        </div>
+    </div>
+    {!! Form::close() !!}
 @endsection
